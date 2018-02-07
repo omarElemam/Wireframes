abe5077325d8434bce9a0b56e773ba8d43dd1b53
Omar Elemam <o.elemam1997@yahoo.com>
2018-02-07 23:09:10 +0200

    Add atm solution

---
+++ atm solution/atm.py
@@ -0,0 +1,25 @@
+def ATM(money , request):
+	if request > money:
+		print "Your Account value is " + str(money)
+		print  "Check your request plz"
+	if request < 0:
+		print "Check your request plz"
+		return
+	while request <= money and request>0:
+		if request >= 100:
+			print "give 100" 
+			request =request - 100
+		elif request >= 50:
+			print "give 50"
+			request =request - 50
+		elif request >= 10:
+			print "give 10"
+			request =request - 10
+		elif request >= 5:
+			print "give 5"
+			request =request - 5
+		else:
+			print "give " + str(request)
+			request = 0
+
+ATM(500, 277)
