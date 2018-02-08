04597dbf77642ea9dfdd535cec80b61245024f94
Omar Elemam <o.elemam1997@yahoo.com>
2018-02-08 23:11:17 +0200

    Add atm_refactored.py

--- atm solution/atm_refactored.py
+++ atm solution/atm_refactored.py
@@ -1,6 +1,7 @@
 def ATM(money , request):
+	print "Current balance : " + str(money)
 	if request > money:
-		print "Your Account value is " + str(money)
+		print "Current balance : " + str(money)
 		print  "Check your request plz"
 	elif request < 0:
 		print "Check your request plz"
@@ -9,17 +10,25 @@ def ATM(money , request):
 		if request >= 100:
 			print "give 100" 
 			request = request - 100
+			money = money - 100
 		elif request >= 50:
 			print "give 50"
 			request = request - 50
+			money = money - request - 50
 		elif request >= 10:
 			print "give 10"
 			request = request - 10
+			money = money - 10
 		elif request >= 5:
 			print "give 5"
 			request = request - 5
+			money = money - 5
 		else:
 			print "give " + str(request)
+			money = money - request
 			request = 0
 
-ATM(500, 273)
+	
+	print "Current balance : " + str(money) 
+
+ATM(500, 204)
