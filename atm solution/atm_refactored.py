0b480a22ed55f3427f1d3600a98d23ad507ba127
Omar Elemam <o.elemam1997@yahoo.com>
2018-02-09 15:06:54 +0200

    Edit atm_refactored.py 3.0

--- atm solution/atm_refactored.py
+++ atm solution/atm_refactored.py
@@ -1,35 +1,37 @@
 def ATM(money , request):
-	print "Current balance : " + str(money)
-	if request > money:
-		print  "Check your request plz"
+	for e in request:
 		print "Current balance : " + str(money)
-		return
-	elif request < 0:
-		print "Check your request plz"
-		return
-	while request <= money and request>0:
-		if request >= 100:
-			print "give 100" 
-			request = request - 100
-			money = money - 100
-		elif request >= 50:
-			print "give 50"
-			request = request - 50
-			money = money - 50
-		elif request >= 10:
-			print "give 10"
-			request = request - 10
-			money = money - 10
-		elif request >= 5:
-			print "give 5"
-			request = request - 5
-			money = money - 5
+		print "===========================" 
+		if e > money:
+			print "Check your request plz"
+		elif e < 0:
+			print "Check your request plz"
 		else:
-			print "give " + str(request)
-			money = money - request
-			request = 0
-
+			while e <= money and e>0:
+				if e >= 100:
+					print "give 100" 
+					e = e - 100
+					money = money - 100
+				elif e >= 50:
+					print "give 50"
+					e = e - 50
+					money = money - 50
+				elif e >= 10:
+					print "give 10"
+					e = e - 10
+					money = money - 10
+				elif e >= 5:
+					print "give 5"
+					e = e - 5
+					money = money - 5
+				else:
+					print "give " + str(request)
+					money = money - e
+					e = 0
 	
 	print "Current balance : " + str(money) 
+	print "===========================" 
 
-ATM(500, 600)
+money = 500
+request = [200, 10, 900, -10, 50, 100, 140, 10]
+ATM(money, request)
