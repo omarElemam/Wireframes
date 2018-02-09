9e1acf4b737b57ba3df235325bd4870f03d1ea96
Omar Elemam <o.elemam1997@yahoo.com>
2018-02-09 17:41:50 +0200

    Edit atm_refactored / Add give_cash function

--- atm solution/atm_refactored.py
+++ atm solution/atm_refactored.py
@@ -1,37 +1,42 @@
 def ATM(money , request):
 	for e in request:
-		print "Current balance : " + str(money)
-		print "===========================" 
 		if e > money:
-			print "Check your request plz"
+			print "please check your request"
 		elif e < 0:
-			print "Check your request plz"
+			print "please check your request"
 		else:
-			while e <= money and e>0:
-				if e >= 100:
-					print "give 100" 
-					e = e - 100
-					money = money - 100
-				elif e >= 50:
-					print "give 50"
-					e = e - 50
-					money = money - 50
-				elif e >= 10:
-					print "give 10"
-					e = e - 10
-					money = money - 10
-				elif e >= 5:
-					print "give 5"
-					e = e - 5
-					money = money - 5
-				else:
-					print "give " + str(request)
-					money = money - e
-					e = 0
-	
-	print "Current balance : " + str(money) 
-	print "===========================" 
+			money = give_cash(e, money)
 
+		print "Current balance : " + str(money) 
+		print "_______________________" 
+
+def give_cash(e, money):
+	while e <= money and e>0:
+		if e >= 100:
+			print "give 100" 
+			e = e - 100
+			money = money - 100
+		elif e >= 50:
+			print "give 50"
+			e = e - 50
+			money = money - 50
+		elif e >= 10:
+			print "give 10"
+			e = e - 10
+			money = money - 10
+		elif e >= 5:
+			print "give 5"
+			e = e - 5
+			money = money - 5
+		else:
+			print "give " + str(e)
+			money = money - e
+			e = 0
+	print "================"
+	return money
+	
+#---------------------------------------------
 money = 500
-request = [200, 10, 900, -10, 50, 100, 140, 10]
+request = [1000, 227, 10, 900, -10, 50, 100, 140, 113]
 ATM(money, request)
+#---------------------------------------------
